2840beb8c3a2e1939d71a36023ccbc10ff43bee0
JohnNCrosby <jcrosby7@mail.csuchico.edu>
2018-02-01 17:37:05 -0800

    Attempt 1

---
+++ to_text.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    x = 0;
+    y = 1;
+    while(y == 1)
+    {
+        cin>>x;
+        if(x == 0)
+        {
+            cout<<"zero\n";
+        }
+        if(x == 1)
+        {
+            cout<<"one\n";
+        }
+        if(x == 2)
+        {
+            cout<<"two\n";
+        }
+        if(x == 3)
+        {
+            cout<<"three\n";
+        }
+        if(x == 4)
+        {
+            cout<<"four\n";
+        }
+        if(x == 5)
+        {
+            cout<<"five\n";
+        }
+        if(x == 6)
+        {
+            cout<<"six\n";
+        }
+        if(x == 7)
+        {
+            cout<<"seven\n";
+        }
+        if(x == 8)
+        {
+            cout<<"eight\n";
+        }
+        if(x == 9)
+        {
+            cout<<"nine\n";
+        }
+    }
+    return 0;
+}
